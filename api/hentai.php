b11ae9735aa51bcc2ca9eb20171514fbc4ee02eb
TuanDeeptry <94787181+kenyrm2250@users.noreply.github.com>
2022-02-06 13:11:34 +0700

    Update hentai.php

--- api/hentai.php
+++ api/hentai.php
@@ -56,12 +56,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $hentai = array(
 "data" => $tuandeeptry,
-"author" => "Tuấn DeepTry",
+"author" => "TuanDeepTry",
 "success" => "true",
 );
 $rdimg = json_encode($hentai);
 
-$i = ['\/v\/','\/','Tuấn DeepTry'];
-$t = ['/v/','/','Tuấn DeepTry'];
+$i = ['\/v\/','\/','TuanDeepTry'];
+$t = ['/v/','/','TuanDeepTry'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
