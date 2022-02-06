c0fb0896fd03bf8113d4cc3e2f94b9ea6a5439be
TuanDeeptry <94787181+kenyrm2250@users.noreply.github.com>
2022-02-06 13:06:58 +0700

    Update cosplay.php

--- api/cosplay.php
+++ api/cosplay.php
@@ -103,12 +103,12 @@ $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $cosplay = array(
 "data" => $tuandeeptry,
-"author" => "Tuấn DeepTry",
+"author" => "TuanDeepTry",
 "success" => "true",
 );
 $rdimg = json_encode($cosplay);
 
-$i = ['\/v\/','\/','Tuấn DeepTry'];
-$t = ['/v/','/','Tuấn DeepTry'];
+$i = ['\/v\/','\/','TuanDeepTry'];
+$t = ['/v/','/','TuanDeepTry'];
 $j = str_replace($i, $t, $rdimg);
 echo $j;
