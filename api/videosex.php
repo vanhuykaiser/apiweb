ab4270068faeec1b5536013a3efca59a34de5e70
TuanDeeptry <94787181+kenyrm2250@users.noreply.github.com>
2022-02-09 03:24:44 +0700

    Add files via upload

--- api/videosex.php
+++ api/videosex.php
@@ -47,11 +47,28 @@ $url = array(
 "https://i.imgur.com/fTrxYYu.mp4",
 "https://i.imgur.com/dUQP0jS.mp4",
 "https://i.imgur.com/cxPMJ0s.mp4",
+"https://i.imgur.com/Bwfcf1E.mp4",
+"https://i.imgur.com/IxuxwUL.mp4",
+"https://i.imgur.com/Wu6Vq4F.mp4",
+"https://i.imgur.com/vN4O25E.mp4",
+"https://i.imgur.com/K1R9Jgs.mp4",
+"https://i.imgur.com/3k3EveT.mp4",
+"https://i.imgur.com/MBEikW1.mp4",
+"https://i.imgur.com/prcCJjD.mp4",
+"https://i.imgur.com/04UQ5kb.mp4",
+"https://i.imgur.com/4XtX34v.mp4",
+"https://i.imgur.com/1kzUVvL.mp4",
+"https://i.imgur.com/SmtVEQR.mp4",
+"https://i.imgur.com/aNciiBh.mp4",
+"https://i.imgur.com/dDHcsfA.mp4",
+"https://i.imgur.com/kJWV8uI.mp4",
+"https://i.imgur.com/QhJ8Xt7.mp4",
+"https://i.imgur.com/PrXHeia.mp4",
 );
 $tuandz = array_rand($url);
 $tuandeeptry = $url [$tuandz];
 $videosex = array(
-"data" => $tuandeeptry,
+"url" => $tuandeeptry,
 "author" => "TuanDeepTry",
 "success" => "true",
 );
