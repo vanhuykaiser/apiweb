9fd97fd91c363eb18ec77e216be8531a490fdb01
TuanDeeptry <94787181+kenyrm2250@users.noreply.github.com>
2022-02-08 12:58:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -578,6 +578,18 @@
                                         </div>
                                     </div>
                                 </div>    
+                                
+                                
+                                <!-- Case Item -->
+                                <div class="col-12 col-lg-6">
+                                    <div class="case-item">
+                                        <img src="https://i.postimg.cc/m2J2g7Vt/659-fucked.gif" height="55px">
+                                        <div>&nbsp;
+                                            <h4 class="title title--h4">API Ảnh Địt Nhau 🌚</h4>
+                                            <button type="button" style="height:30px;width:100px" class="btn btn-primary" onclick="copy('https://tuandz2250api.herokuapp.com/api/ditnhau.php')">COPY</button>
+                                        </div>
+                                    </div>
+                                </div>
 
                           
                         <!-- Clients -->
