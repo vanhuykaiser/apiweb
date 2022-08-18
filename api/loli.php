17d45a8c2125454f65967bb41c6451f877f2978e
TuanDeeptry <94787181+kenyrm2250@users.noreply.github.com>
2022-08-18 18:19:33 +0700

    Add files via upload

---
+++ api/loli.php
@@ -0,0 +1,55 @@
+<?php
+$url = array(
+    "https://i.postimg.cc/KY90YMBY/Amatsukaze-Kantai-Collection-full-1884184.jpg",
+    "https://i.postimg.cc/nc7S1Mbd/Amatsukaze-Kantai-Collection-full-1933504.jpg",
+    "https://i.postimg.cc/Pqf6h4Lc/Amatsukaze-Kantai-Collection-full-1935467.jpg",
+    "https://i.postimg.cc/LXnNg833/ANIME-PICTURES-NET-423110-1379x975-kantai-collection-amatsukaze-destroyer-yukikaze-destroyer-jianr.png",
+    "https://i.postimg.cc/K82qpFmm/ANIME-PICTURES-NET-529820-1150x1600-kantai-collection-shimakaze-destroyer-amatsukaze-destroyer-nez.jpg",
+    "https://i.postimg.cc/tgmzbHGZ/ANIME-PICTURES-NET-639880-2976x4175-original-kase-daiki-tall-image-blush-fringe-short-hair.png",
+    "https://i.postimg.cc/fThfDKpK/ANIME-PICTURES-NET-644520-2976x4175-original-kase-daiki-single-long-hair-tall-image-looking-at-vie.png",
+    "https://i.postimg.cc/YCMRf3x9/ANIME-PICTURES-NET-651902-2000x3000-azur-lane-prinz-eugen-azur-lane-fintowing-single-long-hair-t.jpg",
+    "https://i.postimg.cc/9ffBsQKH/ANIME-PICTURES-NET-651903-2000x3000-azur-lane-prinz-eugen-azur-lane-fintowing-single-long-hair-t.jpg",
+    "https://i.postimg.cc/859wRgyQ/ANIME-PICTURES-NET-660202-1308x2162-original-touwa-nikuman-single-long-hair-tall-image-looking-at.jpg",
+    "https://i.postimg.cc/MZ11PBHj/ANIME-PICTURES-NET-665696-2976x4175-original-kase-daiki-single-long-hair-tall-image-blush.png",
+    "https://i.postimg.cc/W3k0PF7C/ANIME-PICTURES-NET-679666-2976x4175-original-kase-daiki-single-long-hair-tall-image-looking-at-vie.png",
+    "https://i.postimg.cc/SR9dV15X/ANIME-PICTURES-NET-683216-2976x4175-original-karory-long-hair-tall-image-looking-at-viewer-blush.jpg",
+    "https://i.postimg.cc/bwgT6t6R/ANIME-PICTURES-NET-685803-2097x3011-original-unasaka-ryou-single-tall-image-looking-at-viewer-blus.png",
+    "https://i.postimg.cc/pVQnYtcf/ANIME-PICTURES-NET-686091-2976x4175-original-kase-daiki-single-long-hair-tall-image-looking-at-vie.png",
+    "https://i.postimg.cc/59p229kR/ANIME-PICTURES-NET-687506-2976x4175-original-converse-kase-daiki-single-long-hair-tall-image.png",
+    "https://i.postimg.cc/KvZ30sCd/ANIME-PICTURES-NET-692375-2976x4175-original-kase-daiki-single-long-hair-tall-image-looking-at-vie.png",
+    "https://i.postimg.cc/Xq6dNjHJ/ANIME-PICTURES-NET-704544-1290x1913-original-touwa-nikuman-long-hair-tall-image-looking-at-viewer.png",
+    "https://i.postimg.cc/prVVJ3yC/ANIME-PICTURES-NET-706042-2976x4175-original-kase-daiki-tall-image-blush-fringe-short-hair.png",
+    "https://i.postimg.cc/HLKbK4bb/ANIME-PICTURES-NET-713006-641x907-original-miyase-mahiro-single-long-hair-tall-image-looking-at-vi.jpg",
+    "https://i.postimg.cc/hv8xfk60/ANIME-PICTURES-NET-723783-1009x1618-original-touwa-nikuman-single-long-hair-tall-image-looking-at.jpg",
+    "https://i.postimg.cc/zBKHQmz1/ANIME-PICTURES-NET-728288-2097x3011-original-touwa-nikuman-single-long-hair-tall-image-looking-at.png",
+    "https://i.postimg.cc/XqWBS2qB/ANIME-PICTURES-NET-728833-2097x3011-original-touwa-nikuman-long-hair-tall-image-looking-at-viewer.png",
+    "https://i.postimg.cc/Y9nZTPbb/ANIME-PICTURES-NET-742860-2976x4175-original-kase-daiki-single-long-hair-tall-image-blush.png",
+    "https://i.postimg.cc/KvJkwD24/ANIME-PICTURES-NET-749258-3225x2150-azur-lane-manjuu-azur-lane-laffey-azur-lane-laffey-white.jpg",
+    "https://i.postimg.cc/g06xw966/ANIME-PICTURES-NET-750395-2097x3011-original-oywj-single-long-hair-tall-image-looking-at-viewer.jpg",
+    "https://i.postimg.cc/rszXmTKB/ANIME-PICTURES-NET-750510-2894x4417-original-mhru-single-long-hair-tall-image-looking-at-viewer.png",
+    "https://i.postimg.cc/L6X6Ydc6/ANIME-PICTURES-NET-750660-2338x3507-original-teddyellow-single-tall-image-looking-at-viewer-blush.png",
+    "https://i.postimg.cc/vT4kyvKM/ANIME-PICTURES-NET-755019-2976x4175-original-kase-daiki-long-hair-tall-image-looking-at-viewer-fri.png",
+    "https://i.postimg.cc/cHR6mQhx/ANIME-PICTURES-NET-756094-1158x1579-original-touwa-nikuman-long-hair-tall-image-looking-at-viewer.png",
+    "https://i.postimg.cc/PrrNWvwH/ANIME-PICTURES-NET-756874-1040x1600-original-oekakizuki-single-long-hair-tall-image-blush.jpg",
+    "https://i.postimg.cc/GtML7zyC/ANIME-PICTURES-NET-759962-4630x3775-azur-lane-aegir-azur-lane-indomitable-azur-lane-indomitabl.jpg",
+    "https://i.postimg.cc/vmLd2XJN/ANIME-PICTURES-NET-760160-2000x2829-original-gomashio-ponz-single-long-hair-tall-image-looking-at.png",
+    "https://i.postimg.cc/bwnPkq38/ANIME-PICTURES-NET-760679-1075x1518-original-ikeda-yasuhiro-single-long-hair-tall-image-looking-at.jpg",
+    "https://i.postimg.cc/T35d0V2z/EUE5k-M8-Xk-AArdk-P.jpg",
+    "https://i.postimg.cc/R0rMDWNN/EZ38oo-QWo-AM9-Gt9.jpg",
+    "https://i.postimg.cc/zX65yyw0/Kashima-Kantai-Collection-full-1974540.jpg",
+    "https://i.postimg.cc/LsMpVGbP/Murasame-Kantai-Collection-full-2039840.jpg",
+    "https://i.postimg.cc/MZnFvdsJ/amatsukaze-kantai-collection-drawn-by-shin-yashiki-7e15816e305eb618119b60707ea26139.jpg",
+);
+$tuandz = array_rand($url);
+$tuandeeptry = $url [$tuandz];
+$loli = array(
+"data" => $tuandeeptry,
+"author" => "TuanDeepTry",
+"success" => "true",
+);
+$rdimg = json_encode($loli);
+
+$i = ['\/v\/','\/','TuanDeepTry'];
+$t = ['/v/','/','TuanDeepTry'];
+$j = str_replace($i, $t, $rdimg);
+echo $j;
